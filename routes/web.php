6f50206bfbb786c8150c5439ecf4de94b57ef130
Enric Mieza <emieza@xtec.cat>
2021-03-16 00:36:05 +0100

    afegit route::resource de option

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\OptionController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,6 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource("option",OptionController::class);
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
